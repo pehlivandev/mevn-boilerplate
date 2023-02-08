fe5aaeafdf5903f40d6dc028fec667d57fcea139
salih03455 <salih03455@gmail.com>
2023-02-08 15:39:00 +0300

    vue project is created

---
+++ client/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="form">
+      <div>
+        <label>Kelime Ekle</label>
+        <input type="text" />
+      </div>
+      <div>
+        <input type="button" value="Ekle" @click="addWord()" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  function addWord () {
+    alert('add word')
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
